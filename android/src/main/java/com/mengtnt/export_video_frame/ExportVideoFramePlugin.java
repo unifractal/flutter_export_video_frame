52103fec272141b3d2fa16e412888de7c6c714b8
Milad <meladattef2017@gmail.com>
2025-02-21 17:48:27 +0200

    Migrate the plugin to support Flutter V2 embedding.

--- android/src/main/java/com/mengtnt/export_video_frame/ExportVideoFramePlugin.java
+++ android/src/main/java/com/mengtnt/export_video_frame/ExportVideoFramePlugin.java
@@ -1,63 +1,34 @@
-/** 
-MIT License
-
-Copyright (c) 2019 mengtnt
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
 package com.mengtnt.export_video_frame;
 
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Point;
 import android.graphics.PointF;
-import android.graphics.RectF;
 
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Map;
 
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** ExportVideoFramePlugin */
-public class ExportVideoFramePlugin implements MethodCallHandler {
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "export_video_frame");
-    FileStorage.share().setContext(registrar.context());
-    AblumSaver.share().setCurrent(registrar.context());
-    PermissionManager.current().setActivity(registrar.activity());
-    ExportVideoFramePlugin plugin = new ExportVideoFramePlugin();
-    plugin.setRegistrar(registrar);
-    channel.setMethodCallHandler(plugin);
+public class ExportVideoFramePlugin implements FlutterPlugin, MethodCallHandler {
+  private MethodChannel channel;
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "export_video_frame");
+    channel.setMethodCallHandler(this);
   }
 
-  private Registrar registrar;
-
-  public void setRegistrar(Registrar registrar) {
-    this.registrar = registrar;
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+    channel = null;
   }
 
   @Override
@@ -66,7 +37,7 @@ public class ExportVideoFramePlugin implements MethodCallHandler {
       PermissionManager.current().askForPermission();
     }
     if (!(FileStorage.isExternalStorageReadable() && FileStorage.isExternalStorageWritable())) {
-      result.error("File permission exception","Not get external storage permission",null);
+      result.error("File permission exception", "Not get external storage permission", null);
       return;
     }
 
@@ -81,20 +52,20 @@ public class ExportVideoFramePlugin implements MethodCallHandler {
         break;
       }
       case "saveImage": {
-        String filePath = call.argument("filePath").toString();
-        String albumName = call.argument("albumName").toString();
+        String filePath = call.argument("filePath");
+        String albumName = call.argument("albumName");
         Bitmap waterBitMap = null;
         PointF waterPoint = null;
         Double scale = 1.0;
         AblumSaver.share().setAlbumName(albumName);
         if (call.argument("waterMark") != null && call.argument("alignment") != null) {
-          String waterPathKey = call.argument("waterMark").toString();
-          AssetManager assetManager = registrar.context().getAssets();
-          String key = registrar.lookupKeyForAsset(waterPathKey);
-          Map<String,Number> rect = call.argument("alignment");
+          String waterPathKey = call.argument("waterMark");
+          AssetManager assetManager = flutterPluginBinding.getApplicationContext().getAssets();
+          String key = call.argument("waterMark");
+          Map<String, Number> rect = call.argument("alignment");
           Double x = rect.get("x").doubleValue();
           Double y = rect.get("y").doubleValue();
-          waterPoint = new PointF(x.floatValue(),y.floatValue());
+          waterPoint = new PointF(x.floatValue(), y.floatValue());
           Number number = call.argument("scale");
           scale = number.doubleValue();
           try {
@@ -104,57 +75,57 @@ public class ExportVideoFramePlugin implements MethodCallHandler {
             e.printStackTrace();
           }
         }
-        AblumSaver.share().saveToAlbum(filePath,waterBitMap,waterPoint,scale.floatValue(),result);
+        AblumSaver.share().saveToAlbum(filePath, waterBitMap, waterPoint, scale.floatValue(), result);
         break;
       }
       case "exportGifImagePathList": {
-        String filePath = call.argument("filePath").toString();
+        String filePath = call.argument("filePath");
         Number quality = call.argument("quality");
         ExportImageTask task = new ExportImageTask();
-        task.execute(filePath,quality);
+        task.execute(filePath, quality);
         task.setCallBack(new Callback() {
           @Override
           public void exportPath(ArrayList<String> list) {
             if (list != null) {
               result.success(list);
             } else {
-              result.error("Media exception","Get frame fail", null);
+              result.error("Media exception", "Get frame fail", null);
             }
           }
         });
         break;
       }
       case "exportImage": {
-        String filePath = call.argument("filePath").toString();
+        String filePath = call.argument("filePath");
         Number number = call.argument("number");
         Number quality = call.argument("quality");
         ExportImageTask task = new ExportImageTask();
-        task.execute(filePath,number.intValue(),quality);
+        task.execute(filePath, number.intValue(), quality);
         task.setCallBack(new Callback() {
           @Override
           public void exportPath(ArrayList<String> list) {
             if (list != null) {
               result.success(list);
             } else {
-              result.error("Media exception","Get frame fail", null);
+              result.error("Media exception", "Get frame fail", null);
             }
           }
         });
         break;
       }
       case "exportImageBySeconds": {
-        String filePath = call.argument("filePath").toString();
+        String filePath = call.argument("filePath");
         Number duration = call.argument("duration");
         Number radian = call.argument("radian");
         ExportImageTask task = new ExportImageTask();
-        task.execute(filePath,duration.longValue(),radian);
+        task.execute(filePath, duration.longValue(), radian);
         task.setCallBack(new Callback() {
           @Override
           public void exportPath(ArrayList<String> list) {
             if ((list != null) && (list.size() > 0)) {
               result.success(list.get(0));
             } else {
-              result.error("Media exception","Get frame fail", null);
+              result.error("Media exception", "Get frame fail", null);
             }
           }
         });
@@ -164,7 +135,5 @@ public class ExportVideoFramePlugin implements MethodCallHandler {
         result.notImplemented();
         break;
     }
-
   }
-
 }
